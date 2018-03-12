d843911b07c36a7fe2cc2879fe13b249809886e7
LiranCohen <iliran11@gmail.com>
2018-03-12 10:54:37 +0200

    new scripts

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-var cron = require('node-cron');
-const puppeteer = require('puppeteer');
-const moment = require('moment');
+const cron = require('node-cron');
+const script = require('./script.js')
 
 cron.schedule('0 */15 * * * * *', function() {
-  console.log("hello")
-  const currentDate = moment().format('DD-MM-YYYY__HH-mm');
-  const fileName = `${currentDate}.png`;
-  (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});    ;
-    const page = await browser.newPage();
-    await page.goto('https://www1.isracard.co.il/prim3364');
-    await page.screenshot({ path: fileName });
-
-    await browser.close();
-  })();
+  console.log('script ran!')
+  script()
 });
