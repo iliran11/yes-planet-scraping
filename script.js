75f880cd352e014bdab6924ad5f9d6f8f1406999
LiranCohen <iliran11@gmail.com>
2018-03-12 12:38:23 +0200

    local time and save to folder.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer');
-const moment = require('moment');
+const moment = require('moment-timezone');
 module.exports = function() {
-  const currentDate = moment().format('DD-MM-YYYY__HH-mm');
+  const currentDate = moment().tz('Asia/Jerusalem').format('DD-MM-YYYY__HH-mm')
   const fileName = `${currentDate}.png`;
   (async () => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto('https://www1.isracard.co.il/prim3364');
-    await page.screenshot({ path: fileName });
+    await page.screenshot({ path: `./screenshots/${fileName}` });
 
     await browser.close();
   })();
